15724a06811f9eb965b76be39201cb72997ba4c1
kossev89 <kossev89@gmail.com>
2023-02-25 00:24:17 +0200

    Added a new problem and updated README

---
+++ Objects and Classes - Lab/06. Store Boxes/Program.cs
@@ -0,0 +1,44 @@
+﻿string inputData = Console.ReadLine();
+List<Box> boxes = new List<Box>();
+
+while (inputData != "end")
+{
+    string[] inputArray = inputData
+        .Split()
+        .ToArray();
+    int serialNumber = int.Parse(inputArray[0]);
+    string itemName = inputArray[1];
+    int itemQuantity = int.Parse(inputArray[2]);
+    decimal itemPrice = decimal.Parse(inputArray[3]);
+    var box = new Box
+    {
+        SerialNumber = serialNumber,
+        Item = new Item { Name = itemName, Price = itemPrice },
+        ItemQuantity = itemQuantity,
+        BoxPrice = itemQuantity * itemPrice
+    };
+    boxes.Add(box);
+    inputData = Console.ReadLine();
+}
+var sortedByDescendingPrice = boxes.OrderByDescending(x => x.BoxPrice).ToList();
+foreach (var box in sortedByDescendingPrice)
+{
+    Console.WriteLine(box.SerialNumber);
+    Console.WriteLine($"-- {box.Item.Name} – ${box.Item.Price:f2}: {box.ItemQuantity}");
+    Console.WriteLine($"-- ${box.BoxPrice:f2}");
+}
+
+
+class Item
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+class Box
+{
+    public int SerialNumber { get; set; }
+    public Item Item { get; set; }
+    public int ItemQuantity { get; set; }
+    public decimal BoxPrice { get; set; }
+}
--- README.md
+++ README.md
@@ -123,6 +123,18 @@ Use the class from the previous problem. If you receive a student, which already
 should be unique) overwrite the information.
 
 
+2.6 Store Boxes
+Define a class Item, which contains these properties: Name and Price.
+Define a class Box, which contains these properties: Serial Number, Item, Item Quantity and Price for a Box.
+Until you receive "end", you will be receiving data in the following format: "{Serial Number} {Item Name} {Item Quantity} {itemPrice}"
+The Price of one box has to be calculated: itemQuantity * itemPrice.
+Print all the boxes ordered descending by price for a box, in the following format: 
+{boxSerialNumber}
+-- {boxItemName} – ${boxItemPrice}: {boxItemQuantity}
+-- ${boxPrice}
+The price should be formatted to the 2nd digit after the decimal separator.
+
+
 
 
 
