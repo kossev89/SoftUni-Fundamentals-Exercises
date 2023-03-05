714c3f7c0efbb4ffe6bef8fce65fc65bc254929d
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-05 23:47:42 +0200

    Adding a solution for 03. Word Synonyms

---
+++ Lab Dictionaries, Lambda and LINQ/03. Word Synonyms/Program.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+int numberOfWords = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numberOfWords; i++)
+{
+    string word = Console.ReadLine();
+    string synonym = Console.ReadLine();
+    if (!words.ContainsKey(word))
+    {
+        words.Add(word, new List<string>());
+        words[word].Add(synonym);
+    }
+    else
+    {
+        words[word].Add(synonym);
+    }
+}
+
+foreach (var word in words)
+{
+    Console.WriteLine($"{word.Key} - {String.Join(", ", word.Value)}");
+}
+
