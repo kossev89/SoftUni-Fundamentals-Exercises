27e15de8803d6a37cf058cdc0d00f29d2f212d7d
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-06 23:03:19 +0200

    Adding a solution to a problem 01. Count Chars in a String

---
+++ Exercise Associative Arrays/01. Count Chars in a String/Program.cs
@@ -0,0 +1,16 @@
+﻿string input = Console.ReadLine();
+Dictionary<char, int> characters = new Dictionary<char, int>();
+
+foreach (char c in input.Where(x => x != ' '))
+{
+    if (!characters.ContainsKey(c))
+    {
+        characters[c] = 0;
+    }
+    characters[c]++;
+}
+
+foreach (var character in characters)
+{
+    Console.WriteLine($"{character.Key} -> {character.Value}");
+}
