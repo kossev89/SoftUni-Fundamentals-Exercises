f88d615464d6f32a10a04c36967787842c546fa6
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-06 23:18:21 +0200

    Adding a solution to problem 02. A Miner Task

---
+++ Exercise Associative Arrays/02. A Miner Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string recource = Console.ReadLine();
+int quantity = int.Parse(Console.ReadLine());
+Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+
+while (recource != "stop")
+{
+    if (!keyValuePairs.ContainsKey(recource))
+    {
+        keyValuePairs[recource] = 0;
+    }
+    keyValuePairs[recource] += quantity;
+    recource = Console.ReadLine();
+    if (recource=="stop")
+    {
+        break;
+    }
+    quantity = int.Parse(Console.ReadLine());
+}
+
+foreach (var item in keyValuePairs)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
+
