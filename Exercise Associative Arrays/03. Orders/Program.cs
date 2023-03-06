4f521bcf835cff045147578f926ea87152a94bdc
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-06 23:55:53 +0200

    Adding a solution to problem 03. Orders

---
+++ Exercise Associative Arrays/03. Orders/Program.cs
@@ -0,0 +1,26 @@
+﻿Dictionary<string, double[]> productInfo = new Dictionary<string, double[]>();
+string input = Console.ReadLine();
+
+while (input != "buy")
+{
+    string[] infoArray = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+    string productName = infoArray[0];
+    double price = double.Parse(infoArray[1]);
+    double quantity = double.Parse(infoArray[2]);
+
+    if (!productInfo.ContainsKey(productName))
+    {
+        productInfo[productName] = new double[2];
+    }
+
+    productInfo[productName][0] = price;
+    productInfo[productName][1] += quantity;
+    input = Console.ReadLine();
+}
+
+foreach (var product in productInfo)
+{
+    Console.WriteLine($"{product.Key} -> {product.Value[0] * product.Value[1]:f2}");
+}
