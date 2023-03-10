a38953224d27415bb116d8b5d19e74f0450d9c90
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-11 01:09:13 +0200

    Adding a solution to problem 01. Valid Usernames

---
+++ Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+string[] userNames = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+List<string> validUserNames = new List<string>();
+
+foreach (string userName in userNames)
+{
+    if (IsValid(userName))
+    {
+        validUserNames.Add(userName);
+    }
+}
+StringBuilder output = new StringBuilder();
+foreach (var user in validUserNames)
+{
+    output.AppendLine(user);
+}
+Console.WriteLine(output);
+
+
+
+
+
+bool IsValid(string userName)
+{
+    bool isValid = true;
+
+    if (userName.Length < 3 || userName.Length > 16)
+    {
+        isValid = false;
+    }
+    else
+    {
+        for (int i = 0; i < userName.Length; i++)
+        {
+            if (!Char.IsLetter(userName[i]) && !Char.IsDigit(userName[i]) && userName[i] != '-' && userName[i] != '_')
+            {
+                isValid = false;
+                break;
+            }
+            else
+            {
+                isValid = true;
+                continue;
+            }
+        }
+    }
+
+    return isValid;
+}
