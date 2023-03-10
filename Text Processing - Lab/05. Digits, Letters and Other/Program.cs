dbef24c6e1ee8418055ce7c9cb80925a3b43c462
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-10 23:39:37 +0200

    Adding a solution to problem 05. Digits, Letters and Other

---
+++ Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+char[] input = Console.ReadLine().ToCharArray();
+List<char> digits = new List<char>();
+List<char> letters = new List<char>();
+List<char> symbols = new List<char>();
+
+foreach (var item in input)
+{
+    if (Char.IsDigit(item))
+    {
+        digits.Add(item);
+    }
+    else if (Char.IsLetter(item))
+    {
+        letters.Add(item);
+    }
+    else
+    {
+        symbols.Add(item);
+    }
+}
+
+StringBuilder output = new StringBuilder();
+foreach (var digit in digits)
+{
+    output.Append(digit);
+}
+output.AppendLine();
+foreach (var letter in letters)
+{
+    output.Append(letter);
+}
+output.AppendLine();
+foreach (var symbol in symbols)
+{
+    output.Append(symbol);
+}
+
+Console.WriteLine(output);
