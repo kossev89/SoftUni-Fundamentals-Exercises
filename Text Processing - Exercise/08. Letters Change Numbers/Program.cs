21881d4beb9aa097b1524a25fd15a965637d2f77
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-16 23:04:33 +0200

    Adding a solution to problem 08. Letters Change Numbers

---
+++ Text Processing - Exercise/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+double totalSum = 0;
+
+foreach (var item in input)
+{
+    string numberToString = string.Empty;
+    double currentSum = 0;
+    char[] argArray = item.ToCharArray();
+    char firstOperation = argArray[0];
+    char secondOperation = argArray[argArray.Length - 1];
+
+    foreach (var c in argArray)
+    {
+        if (Char.IsDigit(c))
+        {
+            numberToString += c;
+        }
+    }
+
+    double number = double.Parse(numberToString);
+    if (Char.IsUpper(firstOperation))
+    {
+        currentSum += number / (firstOperation - 64);
+    }
+    else if (Char.IsLower(firstOperation))
+    {
+        currentSum += number * (firstOperation - 96);
+    }
+
+    if (Char.IsUpper(secondOperation))
+    {
+        currentSum -= secondOperation - 64;
+    }
+    else if (Char.IsLower(secondOperation))
+    {
+        currentSum += secondOperation - 96;
+    }
+    totalSum += currentSum;
+}
+Console.WriteLine($"{totalSum:f2}");
