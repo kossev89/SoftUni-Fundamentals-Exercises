b136b3494a75d129d06dfd91a96b270949044b18
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-18 00:11:12 +0200

    Adding a solution to problem 02. Match Phone Number

--- Regular Expressions - Lab/01. Match Full Name/Program.cs
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -4,7 +4,6 @@ string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
 string input = Console.ReadLine();
 
 MatchCollection matchedNames = Regex.Matches(input, regex);
-
 foreach (Match match in matchedNames)
 {
     Console.Write($"{match} ");
---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.RegularExpressions;
+
+string regex = @"[+359]{4}([ |-])[\d](\1)[\d]{3}(\1)[\d]{4}\b";
+string input = Console.ReadLine();
+
+MatchCollection validNumber = Regex.Matches(input, regex);
+
+Console.WriteLine(string.Join(", ", validNumber));
