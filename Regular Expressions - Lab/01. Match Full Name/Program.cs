d6b0c4a949e50682aa54de9ec31fec3dbc323648
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-17 23:12:19 +0200

    Adding a solution to problem 01. Match Full Name

---
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+string input = Console.ReadLine();
+
+MatchCollection matchedNames = Regex.Matches(input, regex);
+
+foreach (Match match in matchedNames)
+{
+    Console.Write($"{match} ");
+}
