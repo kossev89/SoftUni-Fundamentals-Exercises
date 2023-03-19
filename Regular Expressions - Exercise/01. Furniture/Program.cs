010bb52459da09e295d76cdc33c1733ca2fcd80e
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-20 01:59:36 +0200

    Adding a solution to problem 01. Furniture

---
+++ Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+var regex = @">{2}(?<furniture>[A-Z]{1}\w+)<{2}(?<price>\d+(?:\.[0-9]*)?)!(?<quantity>\d*)";
+string input = Console.ReadLine();
+List<Match> matches = new List<Match>();
+while (input != "Purchase")
+{
+    if (Regex.IsMatch(input, regex))
+    {
+        matches.Add(Regex.Match(input, regex));
+    }
+    input = Console.ReadLine();
+}
+Console.WriteLine("Bought furniture:");
+decimal totalSum = 0;
+foreach (Match item in matches)
+{
+    string furnitureName = item.Groups["furniture"].Value;
+    decimal price = decimal.Parse(item.Groups["price"].Value);
+    int quantity = int.Parse(item.Groups["quantity"].Value);
+    totalSum += price * quantity;
+    if (quantity > 0 && price > 0)
+    {
+        Console.WriteLine($"{furnitureName}");
+    }
+}
+Console.Write($"Total money spend: {totalSum:f2}");
