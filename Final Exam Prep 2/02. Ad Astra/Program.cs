31c67b980fb9bbec6bda235e3fd0c6545cebdf54
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-27 23:54:49 +0300

    Adding a solution to problem 02. Ad Astra

---
+++ Final Exam Prep 2/02. Ad Astra/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+string pattern = @"(\||\#)(?<item>[^\d\|][A-Za-z]*( )?[A-Za-z]*)\1(?<date>\d{2}\/\d{2}\/\d{2})\1(?<calories>\d+)\1";
+Regex regex = new Regex(pattern);
+string input = Console.ReadLine();
+
+MatchCollection MatchCollection = regex.Matches(input);
+List<StringBuilder> output = new List<StringBuilder>();
+
+int totalCalories = 0;
+
+foreach (Match item in MatchCollection)
+{
+    string foodName = item.Groups["item"].Value.ToString();
+    string expDate = item.Groups["date"].Value.ToString();
+    int calories = int.Parse(item.Groups["calories"].Value);
+    totalCalories += calories;
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.Append($"Item: {foodName}, Best before: {expDate}, Nutrition: {calories}");
+    output.Add(stringBuilder);
+}
+
+int daysToLast = totalCalories / 2000;
+Console.WriteLine($"You have food to last you for: {daysToLast} days!");
+
+foreach (var item in output)
+{
+    Console.WriteLine(item);
+}
+
