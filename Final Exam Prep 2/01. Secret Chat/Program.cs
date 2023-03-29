61d87edaf59a049f01d41fd6fe8254c4fbcc4e0e
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-03-30 00:21:30 +0300

    Adding a solution to problem 01. Secret Chat

---
+++ Final Exam Prep 2/01. Secret Chat/Program.cs
@@ -0,0 +1,44 @@
+﻿string concealedMessage = Console.ReadLine();
+string command = Console.ReadLine();
+string output = concealedMessage;
+
+while (command != "Reveal")
+{
+    string[] commandArg = command.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+    string action = commandArg[0];
+    if (action == "InsertSpace")
+    {
+        int index = int.Parse(commandArg[1]);
+        output = output.Insert(index, " ");
+        Console.WriteLine(output);
+    }
+    else if (action == "Reverse")
+    {
+        string substring = commandArg[1];
+        if (output.Contains(substring))
+        {
+            output = output.Remove(output.IndexOf(substring),substring.Length);
+            char[] substringTocharArray = substring.ToCharArray();
+            Array.Reverse(substringTocharArray);
+            string reversed = new string(substringTocharArray);
+            output = output + reversed;
+        }
+        else
+        {
+            Console.WriteLine("error");
+            Console.WriteLine(output);
+            command = Console.ReadLine();
+            continue;
+        }
+        Console.WriteLine(output);
+    }
+    else if (action == "ChangeAll")
+    {
+        string substring = commandArg[1];
+        string replacement = commandArg[2];
+        output = output.Replace(substring, replacement);
+        Console.WriteLine(output);
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine($"You have a new text message: {output}");
