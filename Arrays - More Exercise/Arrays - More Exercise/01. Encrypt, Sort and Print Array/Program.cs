c8a416e98798387331f0a4dcf4f133154ea59d50
kossev89 <kossev89@gmail.com>
2023-05-09 14:13:57 +0300

    solution to problem 01. Encrypt, Sort and Print

---
+++ Arrays - More Exercise/Arrays - More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+int numberOfInputs = int.Parse(Console.ReadLine());
+int[] outputArray = new int[numberOfInputs];
+for (int i = 0; i < numberOfInputs; i++)
+{
+    int sum = 0;
+    char[] input = Console.ReadLine().ToArray();
+    foreach (char item in input)
+    {
+        if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' || item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+        {
+            sum += item * input.Length;
+        }
+        else
+        {
+            sum += item / input.Length;
+        }
+    }
+    outputArray[i] = sum;
+}
+Array.Sort(outputArray);
+for (int i = 0; i < outputArray.Length; i++)
+{
+    Console.WriteLine(outputArray[i]);
+}
