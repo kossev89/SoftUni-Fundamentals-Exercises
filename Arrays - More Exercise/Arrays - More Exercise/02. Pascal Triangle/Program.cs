4a78c0f3b4845b363a3adaac663a1cc8864b4a1e
kossev89 <kossev89@gmail.com>
2023-05-09 15:42:18 +0300

    Adding a solution to problem 02. Pascal Triangle

---
+++ Arrays - More Exercise/Arrays - More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberOfRows = int.Parse(Console.ReadLine());
+int[] row = new int[] { 1 };
+
+for (int i = 1; i <= numberOfRows; i++)
+{
+    Console.WriteLine(string.Join(' ', row));
+    int[] ints = new int[i + 1];
+    for (int j = 0; j < ints.Length; j++)
+    {
+        if (j == 0 || j == ints.Length - 1)
+        {
+            ints[j] = 1;
+        }
+        else
+        {
+            ints[j] = row[j] + row[j - 1];
+        }
+    }
+    row = ints;
+}
