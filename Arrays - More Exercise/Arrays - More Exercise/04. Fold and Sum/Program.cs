e0feccef960ea193a85c8747ce22563326e21704
kossev89 <kossev89@gmail.com>
2023-05-10 12:33:37 +0300

    Adding a solution to problem 04. Fold and Sum

---
+++ Arrays - More Exercise/Arrays - More Exercise/04. Fold and Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] inputArray = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int[] firstRow = new int[inputArray.Length / 2];
+int[] secondRow = new int[inputArray.Length / 2];
+int[] secondRowFirstHalf = new int[secondRow.Length / 2];
+int[] secondRowSecondHalf = new int[secondRow.Length / 2];
+int delimeter = firstRow.Length / 2;
+firstRow = inputArray.Skip(delimeter).SkipLast(delimeter).ToArray();
+secondRowFirstHalf = inputArray.Take(delimeter).Reverse().ToArray();
+secondRowSecondHalf = inputArray.TakeLast(delimeter).Reverse().ToArray();
+secondRow = secondRowFirstHalf.Concat(secondRowSecondHalf).ToArray();
+int[] output = new int[inputArray.Length / 2];
+
+for (int i = 0; i < output.Length; i++)
+{
+    output[i] = firstRow[i] + secondRow[i];
+}
+
+Console.WriteLine(string.Join(' ', output));
