91a52ea4fc1e40e7c2dd5b39123f7de87afa19de
kossev89 <kossev89@gmail.com>
2023-05-10 10:30:45 +0300

    Adding a solution to problem 03. Recursive Fibonacci

---
+++ Arrays - More Exercise/Arrays - More Exercise/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine());
+double[] sequence = new double[n+1];
+sequence[0] = 1;
+sequence[1] = 1;
+for (int i = 2; i < n; i++)
+{
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+}
+Console.WriteLine(sequence[n - 1]);
